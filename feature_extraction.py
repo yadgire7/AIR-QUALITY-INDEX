0be75ddd406888b812967ee5e454b269b997b7fb
yadgire7 <47882001+yadgire7@users.noreply.github.com>
2023-06-13 00:13:34 -0700

    Update feature_extraction.py

--- feature_extraction.py
+++ feature_extraction.py
@@ -10,6 +10,7 @@ import pandas as pd
 from matplotlib import pyplot as plt
 import hopsworks
 import xgboost
+import streamlit as st
 
 os.environ['PYSPARK_PYTHON'] = sys.executable
 os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
@@ -92,5 +93,82 @@ joined_df2 = joined_df1.join(aqi_df, on='idx',how='inner')
 
 main_df = joined_df2.toPandas()
 
-# imputing the nan in aqi with the mean of the aqi
-main_df['aqi'] = main_df['aqi'].fillna(main_df['aqi'].mean())
+def impute_null(main_df, cols):
+    for col in cols:
+        main_df[col] = main_df[col].fillna(main_df[col].mean())
+    return main_df
+
+main_df = impute_null(main_df, list(main_df.columns))
+
+project = hopsworks.login()
+fs = project.get_feature_store()
+
+aqi_fg = fs.get_or_create_feature_group(
+    name="air_quality_index",
+    version="1",
+    description="Air Quality Index",
+    primary_key=['idx']
+)
+
+aqi_fg.insert(main_df)
+
+feature_descriptions = [
+    {"name": "idx", "description": "City ID"},
+    {"name": "latitude", "description": "Latitude of location"},
+    {"name": "longitude", "description": "Longitude of location"},
+    {"name": "p", "description": "pressure"},
+    {"name": "h", "description": "humidity"},
+    {"name": "pm25", "description": "pm2.5"},
+    {"name": "pm10", "description": "pm1.0"},
+    {"name": "o3", "description": "ozone"},
+    {"name": "no2", "description": "nitrogen dioxide"},
+    {"name": "so2", "description": "sulphur dioxide"},
+    {"name": "aqi", "description": "air quality index"}
+    
+]
+# idx	latitude	longitude	p	h	t	pm25	pm10	o3	no2	so2	aqi
+for desc in feature_descriptions: 
+    aqi_fg.update_feature_description(desc["name"], desc["description"])
+    
+ds_query = aqi_fg.select(["idx",	"latitude",	"longitude",	"p",	"h",	"t",    	"pm25",	"pm10",	"o3",	"no2",	"so2",	"aqi"])
+
+try:
+    fv_name= 'aqi_view'+str(random.randint(1,1000))
+    feature_view = fs.get_feature_view(fv_name, version=1)
+except:
+    fv_name= 'aqi_view'+str(random.randint(1,1000))
+    feature_view = fs.create_feature_view(
+        name=fv_name,
+        query=ds_query,
+        version=1,
+        labels=["aqi"]
+    )
+    
+aiq_version, aiq_job = feature_view.create_train_validation_test_split(
+    description = 'training set',
+    data_format = 'csv',
+    validation_size = 0.1,
+    test_size = 0.1,
+    write_options = {'wait_for_job': True},
+    coalesce = True,
+)
+
+X_train, X_val, X_test, y_train, y_val, y_test = feature_view.get_train_validation_test_split(1)
+
+# train the model
+mod = xgboost.XGBRegressor()
+mod = mod.fit(X_train, y_train)
+
+pred = mod.predict(X_val)
+
+st.title('AQI Prediction')
+def reflect_action(action):
+    if action == "Show dataframe":
+        st.write(ds_query.show(5))
+    elif action == "SHow columns":
+        st.write(main_df.columns)
+    elif action == "Show metadata":
+        st.write(main_df.describe())
+
+action = st.sidebar.selectbox("Select action", ("Show dataframe","Show columns","Show metadata"))
+reflect_action(action)
